11ca7116528db742c413555364ea63331ff58359
fadishouhfa <fadishouhfa@gmail.com>
2025-03-03 13:42:04 +0300

    Working On Models

--- api/models.py
+++ api/models.py
@@ -1,4 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser( AbstractUser ) :   
+    phone_number = models.CharField( max_length=17, blank=True )
+    address = models.TextField()
+    profile_image = models.ImageField( upload_to='profile_images/', null=True, blank=True ) 
+    created_at = models.DateTimeField( auto_now_add=True )
+    updated_at = models.DateTimeField( auto_now=True )
 
 class LocatedCity( models.Model ) :
     name = models.CharField( max_length = 20 ) 
@@ -35,8 +43,28 @@ class Product( models.Model ) :
     description = models.TextField() 
     constraints = models.ManyToManyField( ProductConstraint , related_name = 'Product-Constraints' ) 
     needs_perscription = models.BooleanField( default = False ) 
+    def __str__( self ) :
+        return self.name 
     
-    
+class Pharmacy( models.Model ) :
+    owner = models.OneToOneField( CustomUser , related_name = 'pharmacy-owner' , on_delete = models.CASCADE ) 
+    name = models.CharField( max_length = 200 )
+    located_city = models.ForeignKey( LocatedCity , on_delete = models.CASCADE ) 
+    address = models.TextField()
+    def __str__( self ) :
+        return self.name 
 
+class PharmacyStorageOfProducts( models.Model ) :
+    pharmacy = models.ForeignKey( Pharmacy , on_delete = models.CASCADE , related_name = 'pharmacy-storage' ) 
+    products = models.ManyToManyField( Product , related_name = 'linking-products' ) 
+    count = models.IntegerField( default = 0 )  
+    
+class Envoice( models.Model ) :
+    pharmacy = models.ForeignKey( Pharmacy , on_delete = models.CASCADE , related_name = 'pharmacy-envoice' ) 
+    products = models.ManyToManyField( Product , related_name = 'envoice-products' ) 
+    count = models.IntegerField( default = 1 ) 
+    date = models.DateTimeField( auto_now_add = True ) 
+    is_checked = models.BooleanField( default = False )
+        
     
     
