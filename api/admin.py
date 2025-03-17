7fb46d61a2f8f1d68f169773d36b80d0b5d3ee32
fadishouhfa <fadishouhfa@gmail.com>
2025-03-17 10:57:48 +0300

    Fixes

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register( LocatedCity ) 
+admin.site.register( ManufacturingLaboratory ) 
+admin.site.register( ProductClassification ) 
+admin.site.register( ProductConstraint ) 
+admin.site.register( ProductType ) 
+admin.site.register( Product ) 
+admin.site.register( Pharmacy ) 
+admin.site.register( PharmacyStorageOfProducts ) 
+admin.site.register( InvoiceItem ) 
+admin.site.register( Invoice )   
--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path 
-
+from .views import * 
 urlpatterns = [
-
+    #Located Cities 
+    path( 'cities' , ListCreateLocatedCityView.as_view() , name = 'cities' ) ,
+    path( 'cities/<int:pk>' , LocatedCityDetailView.as_view() , name = 'city-detail' ) ,
+    #Laboratories 
+    path( 'labs' , ListCreateManufacturingLaboratoryView.as_view() , name = 'labs' ) ,
+    path( 'labs/<int:pk>' , ManufacturingLaboratoryDetailView.as_view() , name ='lab-detail' ) ,
 ]
